a29a7d167aa79be732e6fed088ed190714195515
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-10-01 13:30:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-
-             
-              import {initializeApp} from "firebase/app";
-              import {getAuth} from "firebase/auth";
-             
-            
-              const firebaseConfig = {
-                apiKey: "AIzaSyDVRZOdK-zJrvCVv6kaqgYY8r7oMWe-eOk",
-              authDomain: "ai-trip-planner-40b66.firebaseapp.com",
-              projectId: "ai-trip-planner-40b66",
-              storageBucket: "ai-trip-planner-40b66.appspot.com",
-              messagingSenderId: "458700712552",
-              appId: "1:458700712552:web:91049aaf085c0d11a369dd",
-              measurementId: "G-78QLP59M96"
+const firebaseConfig = {
+  apiKey: <YOURAPIKEY>,
+  authDomain: <YOURAUTHDOAMIN>,
+  projectId: <YOURPROJECTID>,
+  storageBucket: <YOURSTORAGEBUCKET>,
+  messagingSenderId: <YOURMESSAGINGSENDERID>,
+  appId: <YOURAPPID>,
 };
 
-              // Initialize Firebase
-              const app = initializeApp(firebaseConfig);
-              export const auth = getAuth(app);
-              export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
